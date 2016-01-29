60b6ea6f7800ba7678b136ba9aaa5a68db85d4b2
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-01-29 19:38:12 -0300

    global _Log static class

--- src/tsadmlog/__init__.py
+++ src/tsadmlog/__init__.py
@@ -3,18 +3,24 @@ import time
 
 from tsadmcfg import TSAdmCfg
 
+class _Log:
+    cfg = None
+    ftime = None
+    initDone = False
+
 class TSAdmLogger:
-    _cfg = None
-    _ftime = None
     _caller = None
 
     def __init__(self, caller):
-        self._cfg = TSAdmCfg()
-        self._ftime = self._cfg.get('LOG_FTIME')
+        if not _Log.initDone:
+            _Log.cfg = TSAdmCfg()
+            _Log.ftime = _Log.cfg.get('LOG_FTIME')
+            _Log.initDone = True
+            self._print('log init done')
         self._caller = caller
 
     def _print(self, *msg):
-        print('[{}] {}:'.format(time.strftime(self._ftime), self._caller), *msg, file=sys.stderr)
+        print('[{}] {}:'.format(time.strftime(_Log.ftime), self._caller), *msg, file=sys.stderr)
 
     def debug(self, *msg):
         self._print(*msg)
