d00ac53aa5070dbf46568a74d95f151c995a6daa
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-01-29 00:58:13 -0300

    TSAdmUser initial commit

--- src/tsadm/__init__.py
+++ src/tsadm/__init__.py
@@ -1,10 +1,13 @@
 from tsadmcfg import getConfig
 from tsadmdb import getDB
+from tsadmuser import TSAdmUser
 
 class TSAdm:
     cfg = None
     db = None
+    user = None
 
     def __init__(self):
         self.cfg = getConfig()
         self.db = getDB()
+        self.user = TSAdmUser()
