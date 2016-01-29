fc567e91b4de9c52b99ea20eba75d220c91f3fb5
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-01-29 12:57:47 -0300

    auth views

--- src/tsadm/urls.py
+++ src/tsadm/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from .views import HomeView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view()),
+    url(r'^u/', include('tsadmuser.urls', namespace='user')),
     url(r'^admin/', admin.site.urls),
 ]
--- src/tsadmviews/__init__.py
+++ src/tsadmviews/__init__.py
@@ -1,4 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-class TSAdmView(TemplateView):
+class TSAdmView(LoginRequiredMixin, TemplateView):
     pass
