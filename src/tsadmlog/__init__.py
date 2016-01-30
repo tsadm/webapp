bc21956a6714202f35c93604059f6bd82b406649
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-01-29 21:11:57 -0300

    set  __name__ as default caller

--- src/tsadmlog/__init__.py
+++ src/tsadmlog/__init__.py
@@ -9,14 +9,14 @@ class _Log:
     initDone = False
 
 class TSAdmLogger:
-    _caller = None
+    _caller = __name__
 
     def __init__(self, caller):
         if not _Log.initDone:
             _Log.cfg = TSAdmCfg()
             _Log.ftime = _Log.cfg.get('LOG_FTIME')
             _Log.initDone = True
-            self._print('log init done')
+            self._print('init done')
         self._caller = caller
 
     def _print(self, *msg):
