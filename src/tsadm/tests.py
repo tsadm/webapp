8195584d5571a52e2b439ec75ba7bfeac5f94e5a
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-02-01 18:19:48 -0300

    logged in tests

--- src/tsadm/tests.py
+++ src/tsadm/tests.py
@@ -1,7 +1,28 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-class TSAdmTest(TestCase):
+USER_NAME = 'tester'
+USER_PASS = 'gyLwHEberRq43ohP'
+
+class TSAdmAnonTest(TestCase):
     def test_HomeView(self):
         resp = self.client.get(reverse('home'))
         self.assertRedirects(resp, '{}?next=/'.format(reverse('login')))
+
+class TSAdmTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username=USER_NAME, password=USER_PASS)
+        self.client.force_login(self.user)
+
+    def test_HomeView(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_HttpOptions(self):
+        resp = self.client.options(reverse('home'))
+        self.assertEqual(resp.get('Allow'), 'GET, HEAD, OPTIONS')
+
+    def test_HttpCharset(self):
+        resp = self.client.head(reverse('home'))
+        self.assertEqual(resp.charset, 'utf-8')
