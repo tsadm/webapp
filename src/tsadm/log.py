981db9a7aacea2a523f55b80e9d7a0a4e0321f8d
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-02-01 21:15:59 -0300

    set log level from config

--- src/tsadm/log.py
+++ src/tsadm/log.py
@@ -3,9 +3,17 @@ import time
 
 from .config import TSAdmCfg
 
+_LEVELS = {
+    'OFF': 0,
+    'DEBUG': 1,
+    'WARNING': 2,
+    'ERROR': 3,
+}
+
 class _Log:
     cfg = None
     ftime = None
+    level = None
     initDone = False
 
 class TSAdmLogger:
@@ -15,15 +23,16 @@ class TSAdmLogger:
         if not _Log.initDone:
             _Log.cfg = TSAdmCfg()
             _Log.ftime = _Log.cfg.get('LOG_FTIME')
+            _Log.level = _Log.cfg.get('LOG_LEVEL', 'DEBUG')
             _Log.initDone = True
-            self._print('init done')
         self._caller = caller
 
-    def _print(self, *msg):
-        print('[{}] {}:'.format(time.strftime(_Log.ftime), self._caller), *msg, file=sys.stderr)
+    def _print(self, *msg, level=None):
+        if _LEVELS.get(level) <= _LEVELS.get(_Log.level):
+            print('[{}] {} - {}:'.format(time.strftime(_Log.ftime), level, self._caller), *msg, file=sys.stderr)
 
     def debug(self, *msg):
-        self._print(*msg)
+        self._print(*msg, level='DEBUG')
 
     def error(self, *msg):
-        self._print('ERROR:', *msg)
+        self._print(*msg, level='ERROR')
