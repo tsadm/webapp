e453074ad86c8ceb8620755a407de8d1119952ad
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-02-03 04:30:53 -0300

    test login and logout views

--- src/tsadm/tests/test_anon.py
+++ src/tsadm/tests/test_anon.py
@@ -1,7 +1,18 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 class TSAdmAnonTest(TestCase):
     def test_LoginRedirect(self):
         resp = self.client.get(reverse('home'))
         self.assertRedirects(resp, '{}?next=/'.format(reverse('login')))
+
+    def test_LoginLogout(self):
+        credentials = dict(username='tester', password='izkK9aAfuppnmrMi')
+        user = User.objects.create_user(**credentials)
+        # login
+        resp = self.client.post(reverse('login'), credentials)
+        self.assertRedirects(resp, reverse('home'))
+        # logout
+        resp = self.client.get(reverse('logout'))
+        self.assertRedirects(resp, reverse('login'))
