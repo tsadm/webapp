ea4f90079a5593e718a9d223176639110499aac4
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-02-03 00:00:11 -0300

    create a fakesite on TSAdmTest setUp

--- src/tsadmsite/tests/test.py
+++ src/tsadmsite/tests/test.py
@@ -1,6 +1,12 @@
 from tsadm.tests import TSAdmTestBase
+from ..models import TSAdmSiteDB
 
 class TSAdmTest(TSAdmTestBase):
+    def setUp(self):
+        super(TSAdmTest, self).setUp()
+        site = TSAdmSiteDB(name='fakesite')
+        site.save()
+
     def test_SiteView(self):
         resp = self.client.get(self.getURL('site:home', kwargs={'name': 'fakesite'}))
         self.assertEqual(resp.status_code, 200)
