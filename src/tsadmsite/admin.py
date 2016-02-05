a04528db112a4ae37ca11181bd29ff21126ac802
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-02-04 22:31:05 -0300

    site env db model

--- src/tsadmsite/admin.py
+++ src/tsadmsite/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import TSAdmSiteDB
+from .models import TSAdmSiteDB, TSAdmSiteEnvDB
 
 class SiteAdmin(admin.ModelAdmin):
     pass
 
+class SiteEnvAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(TSAdmSiteDB, SiteAdmin)
+admin.site.register(TSAdmSiteEnvDB, SiteEnvAdmin)
--- src/tsadmsite/models.py
+++ src/tsadmsite/models.py
@@ -9,3 +9,21 @@ class TSAdmSiteDB(models.Model):
 
     def __str__(self):
         return "Site:"+self.name
+
+class TSAdmSiteEnvDB(models.Model):
+    class Meta:
+        verbose_name = "SiteEnv"
+        verbose_name_plural = "SiteEnvs"
+
+    name = models.CharField(max_length=64, unique=True)
+    site = models.ForeignKey(
+        TSAdmSiteDB,
+        on_delete=models.PROTECT,
+    )
+    host = models.ForeignKey(
+        'tsadmhost.TSAdmHostDB',
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return "SiteEnv:"+self.name
