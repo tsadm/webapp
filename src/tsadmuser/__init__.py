4eec7f40850ce2c47b5e46a65e25867817a24b93
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-07 21:04:53 -0300

    TSAdmUser siteEnvs list method

--- src/tsadmuser/__init__.py
+++ src/tsadmuser/__init__.py
@@ -4,12 +4,18 @@ from django.core.exceptions import ObjectDoesNotExist
 logger = TSAdmLogger(__name__)
 
 class TSAdmUser:
+    _db = None
+
     def load(self, django_user):
         logger.debug('load django user:', django_user)
+        self._db = django_user
         try:
-            return django_user.tsadmuser
+            _ = django_user.tsadmuser
         except ObjectDoesNotExist:
             from tsadmuser.models import TSAdmUserDB
             logger.warning('user not initialized')
             u = TSAdmUserDB(user=django_user)
             return u.save()
+
+    def siteEnvs(self):
+        return self._db.tsadmuser.siteenv.all()
