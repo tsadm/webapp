5541c24266b577b40ad7e96d73de5cf73451adb2
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-08 02:45:44 -0300

    test home view sites listing

--- src/tsadm/tests/test_views.py
+++ src/tsadm/tests/test_views.py
@@ -4,4 +4,6 @@ class TSAdmViewsTest(TSAdmTestBase):
 
     def test_HomeView(self):
         resp = self.client.get(self.getURL('home'))
-        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'TEST:site:s0', count=1, status_code=200)
+        self.assertContains(resp, 'TEST:site:s1', count=1, status_code=200)
+        self.assertNotContains(resp, 'TEST:site:s2', status_code=200)
