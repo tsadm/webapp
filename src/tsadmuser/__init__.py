34dfdc5e756c58b104f9fc21174c63101f766ae1
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-08 01:46:22 -0300

    TSAdmUser sites and siteEnvs methods

--- src/tsadmuser/__init__.py
+++ src/tsadmuser/__init__.py
@@ -1,5 +1,6 @@
 from tsadm.log import TSAdmLogger
 from django.core.exceptions import ObjectDoesNotExist
+from tsadmsite import TSAdmSite
 
 logger = TSAdmLogger(__name__)
 
@@ -17,5 +18,15 @@ class TSAdmUser:
             u = TSAdmUserDB(user=django_user)
             return u.save()
 
-    def siteEnvs(self):
-        return self._db.tsadmuser.siteenv.all()
+    def sites(self):
+        sites = list()
+        prevId = None
+        for env in self._db.tsadmuser.siteenv.order_by('site__name', 'name'):
+            sid = env.site.id
+            if sid != prevId:
+                sites.append(TSAdmSite(env.site))
+            prevId = sid
+        return sites
+
+    def siteEnvs(self, siteId):
+        return self._db.tsadmuser.siteenv.filter(site__id=siteId).order_by('site__name', 'name').values()
