73aa3c676fa8605fde15fb55a7a9287e8f77ea8a
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-15 01:29:41 -0300

    fix auth templates themed paths

--- src/tsadm/auth_urls.py
+++ src/tsadm/auth_urls.py
@@ -16,25 +16,26 @@ urlpatterns = [
 
     # password change
     url(r'^password_change/$', auth_views.password_change,
-        {'template_name': 'tsadm/password_change_form.html'}, name='password_change'),
+        {'template_name': tmplPath('tsadm/password_change_form.html')},
+        name='password_change'),
     url(r'^password_change/done/$', auth_views.password_change_done,
-        {'template_name': 'tsadm/password_change_done.html'},
+        {'template_name': tmplPath('tsadm/password_change_done.html')},
         name='password_change_done'),
 
     # password reset
     url(r'^password_reset/$', auth_views.password_reset,
-        {'template_name': 'tsadm/password_reset_form.html',
-        'email_template_name': 'tsadm/password_reset_email.html',
-        'subject_template_name': 'tsadm/password_reset_subject.txt',},
+        {'template_name': tmplPath('tsadm/password_reset_form.html'),
+        'email_template_name': tmplPath('tsadm/password_reset_email.html'),
+        'subject_template_name': tmplPath('tsadm/password_reset_subject.txt'),},
         name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done,
-        {'template_name': 'tsadm/password_reset_done.html'},
+        {'template_name': tmplPath('tsadm/password_reset_done.html')},
         name='password_reset_done'),
 
     # reset confirm
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,
-        {'template_name': 'tsadm/password_reset_confirm.html'},
+        {'template_name': tmplPath('tsadm/password_reset_confirm.html')},
         name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete,
-        {'template_name': 'tsadm/password_reset_complete.html'}, name='password_reset_complete'),
+        {'template_name': tmplPath('tsadm/password_reset_complete.html')}, name='password_reset_complete'),
 ]
