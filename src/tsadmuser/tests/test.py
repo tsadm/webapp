6fedd5a1bb0c8795b435373d2b1212eda5338066
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-15 01:13:43 -0300

    check user name count=1 and check status code

--- src/tsadmuser/tests/test.py
+++ src/tsadmuser/tests/test.py
@@ -3,5 +3,4 @@ from tsadm.tests import TSAdmTestBase
 class TSAdmUserTest(TSAdmTestBase):
     def test_UserView(self):
         resp = self.client.get(self.getURL('user:home'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertContains(resp, 'TEST:user.name:tester')
+        self.assertContains(resp, 'TEST:user.name:tester', count=1, status_code=200)
