849eb75a2f0d3a688c85413ba680c1d97803ace3
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-17 15:46:27 -0300

    force slash at end of site env URLs

--- src/tsadmsite/urls.py
+++ src/tsadmsite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import SiteView, SiteEnvView
 
 urlpatterns = [
+    url(r'(?P<site>[a-zA-Z0-9]+)/(?P<env>[a-zA-Z0-9]+)/$', SiteEnvView.as_view(), name='env'),
     url(r'(?P<name>[a-zA-Z0-9]+)/$', SiteView.as_view(), name='home'),
-    url(r'(?P<site>[a-zA-Z0-9]+)/(?P<env>[a-zA-Z0-9]+)$', SiteEnvView.as_view(), name='env'),
 ]
