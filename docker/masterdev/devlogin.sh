35756ff5c164447a1fab7a0e819e62121045aa6c
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-19 17:12:46 -0300

    split master dev docker img files

--- docker/masterdev/devlogin.sh
+++ docker/masterdev/devlogin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 set -e
+groupmod -g ${DOCKER_GID} tsadm
+usermod -g ${DOCKER_GID} -u ${DOCKER_UID} tsadm
 umask ${DOCKER_UMASK}
-groupadd -g ${DOCKER_GID} tsadm
-useradd -c tsadm -m -g ${DOCKER_GID} -u ${DOCKER_UID} -s /usr/sbin/nologin tsadm
 set +e
 exec su -s /bin/bash -l tsadm
--- mk/env.mk
+++ mk/env.mk
@@ -1,9 +1,10 @@
 DOCKER_IMG ?= tsadm/master
-DOCKER_NAME ?= tsadm-master
+DOCKER_DEVIMG ?= tsadm/masterdev
+DOCKER_MASTER_NAME ?= tsadm-master
 DOCKER_CMD ?=
-TEST_SUITE ?=
-TSADM_LOG ?= off
 DOCKER_UID ?= $(shell id -u)
 DOCKER_GID ?= $(shell id -g)
 DOCKER_USER ?= $(DOCKER_UID):$(DOCKER_GID)
 DOCKER_UMASK ?= $(shell umask)
+TEST_SUITE ?=
+TSADM_LOG ?= off
