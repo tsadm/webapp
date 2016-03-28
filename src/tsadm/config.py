78ae971d17d693243ad0c39969ed089a91346915
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-03-28 14:33:53 -0300

    dump config if executed

--- src/tsadm/config.py
+++ src/tsadm/config.py
@@ -1,10 +1,21 @@
+import os.path
+
+_RUN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 _CFG = {
     'LOG_LEVEL': 'OFF',
     'LOG_FTIME': '%d/%b/%Y %H:%M:%S',
     'TEMPLATES_THEME': 'devel',
     'JSON_PRETTY_PRINT': True,
+    'RUN_DIR': _RUN_DIR,
 }
+
 
 class TSAdmCfg:
     def get(self, name, default=None):
         return _CFG.get(name, default)
+
+
+if __name__ == '__main__':
+    import json
+    print(json.dumps(_CFG, sort_keys=True, indent=4))
