e4d99865edfc00c271bf34973d8900dfef4fa192
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-04-15 13:52:00 -0300

    show db connections info

--- src/tsadm/views/__init__.py
+++ src/tsadm/views/__init__.py
@@ -1,4 +1,5 @@
 import tsadmuser
+import django.db
 from .base import TSAdmView
 from ..log import TSAdmLogger
 
@@ -27,8 +28,26 @@ class InfoView(TSAdmView):
         logger.debug('InfoView init')
         super(InfoView, self).__init__()
 
+    def _dbconns(self):
+        dbConnections = list()
+        for dbconn in django.db.connections.all():
+            dbinfo = {
+                'alias': dbconn.alias,
+                'version': dbconn.Database.version,
+                'name': dbconn.settings_dict['NAME'],
+                'vendor': dbconn.vendor,
+                'vendorVersion': None,
+            }
+            if dbconn.vendor == 'sqlite':
+                dbinfo['vendorVersion'] = dbconn.Database.sqlite_version
+            dbConnections.append(dbinfo)
+        return dbConnections
+
     def get_context_data(self, **kwargs):
         logger.debug('get_context_data')
         context = super(InfoView, self).get_context_data(**kwargs)
-        context['tsadm'] = self.tsadm
+        context['tsadm'] = {
+            'config': self.tsadm.cfg.dumps(),
+            'dbConnections': self._dbconns(),
+        }
         return context
